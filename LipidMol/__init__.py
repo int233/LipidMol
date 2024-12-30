5aec13f77b8b7e8f3d0306bb02895800f3c2fa28
int0030 <int0030@163.com>
2024-12-30 21:50:25 +0800

    setup.py

--- LipidMol/__init__.py
+++ LipidMol/__init__.py
@@ -1,3 +1,4 @@
 from .lipidmolecular import FormulaParser
 
 __all__ = ['FormulaParser']
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LipidMol",
+    version="0.1.0",
+    description="Calculate the exact mass of lipids based on their common names",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="int0030",
+    author_email="int0030@163.com",
+    url="https://github.com/int233/LipidMol",
+    packages=find_packages(),
+    install_requires=[
+        "molmass"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires='>=3.8',
+    include_package_data=False,
+)
